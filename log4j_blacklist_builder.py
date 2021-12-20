1f23c676ae620388a0e81d4f6407b1e75b8a10cd
Gianluca Ulivi <gianlu9858@gmail.com>
2021-12-20 14:20:29 +0100

    Added support for local file as source

--- log4j_blacklist_builder.py
+++ log4j_blacklist_builder.py
@@ -1,3 +1,4 @@
+from typing import Counter
 import requests
 import csv
 import datetime
@@ -13,19 +14,21 @@ class SmartFormatter(argparse.HelpFormatter):
         return argparse.HelpFormatter._split_lines(self, text, width)
 
 #Variable definitions
-url_gnremy = 'https://gist.githubusercontent.com/gnremy/c546c7911d5f876f263309d7161a7217/raw'
+url_gnremy = 'https://gist.githubusercontent.com/gnremy/c546c7911d5f876f263309d7161a7217'
 url_CPS = 'https://raw.githubusercontent.com/CriticalPathSecurity/Public-Intelligence-Feeds/master/log4j.txt'
 current_time = datetime.datetime.now().strftime("%Y-%b-%d_%H-%M")
 IP_count = 0
 IP_list = []
+source_is_local_file = False
 
 #Argument Parser and help
 parser = argparse.ArgumentParser(formatter_class=SmartFormatter)
 parser.add_argument('source', type=str, 
     help='R|Source to gather IP List\n'
-        'gnremy:     https://gist.githubusercontent.com/gnremy/c546c7911d5f876f263309d7161a7217 \n'
-        'CPS:        https://raw.githubusercontent.com/CriticalPathSecurity/Public-Intelligence-Feeds/master/log4j.txt \n'
-        'Custom URL: Insert your customer URL to gather IP List, must be a raw IP list (Like CPS)'
+        'gnremy:     ' + url_gnremy + '\n'
+        'CPS:        ' + url_CPS + '\n'
+        'Custom URL: Insert your custom URL to gather IP List, must be a raw IP list \n'
+        'Local File: Path to your custom IP List File, must be a raw IP list'
         )
 parser.add_argument('format', type=str,
     help='R|Firewall Format to generate the configuration \n'
@@ -43,23 +46,37 @@ if firewall_format.upper() != 'ASA' and firewall_format.upper() != 'LIST':
 #URL Setting based on source argument
 if IP_source == 'gnremy':
     URL_source = 'gnremy'
-    url = url_gnremy
+    url = url_gnremy + '/raw'
 elif IP_source == 'CPS':
     URL_source = 'CPS'
     url = url_CPS
-else:
+elif IP_source.startswith('http://') or IP_source.startswith('https://'):
+    print('IP Source is a Custom URL.')
     URL_source = 'CUSTOM_URL'
     url = IP_source
+else:
+    print('IP Source is a local file.')
+    URL_source = 'LOCAL_FILE'
+    path = IP_source
+    source_is_local_file = True
 
-#Validating the URL and getting response
-try:
-    response = requests.get(url)
-    response.raise_for_status()
-    IP_list = response.text.splitlines()
-except requests.exceptions.HTTPError as error:
-    raise SystemExit(error)
-except requests.exceptions.RequestException as error:
-    raise SystemExit(error)
+if source_is_local_file:
+    #Get IP List from local file
+    try:
+        file_source = open(path, 'r')
+        IP_list = file_source.readlines()
+    except OSError as error:
+        raise SystemExit(error)
+else:
+    #Validating the URL and getting response
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        IP_list = response.text.splitlines()
+    except requests.exceptions.HTTPError as error:
+        raise SystemExit(error)
+    except requests.exceptions.RequestException as error:
+        raise SystemExit(error)
 
 #Response parsing for gnremy source from csv to list
 if IP_source == 'gnremy':
@@ -90,7 +107,7 @@ if firewall_format.upper() == 'LIST':
 if firewall_format.upper() == 'ASA':
     file.write("object-group network Log4j_Blacklist_IP \n")
     for line in IP_list:
-        file.write("network-object host " + line + "\n")
+        file.write("network-object host " + line.rstrip() + "\n")
         IP_count += 1
 
 print('Total IP from Source: ' + str(IP_count))
